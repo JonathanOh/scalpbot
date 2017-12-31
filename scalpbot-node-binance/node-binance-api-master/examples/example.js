ddbe9b7bda1fc9eaed4ca67bb51596657239b2a2
Unknown <martyt1985@bellsouth.net>
2017-12-31 01:16:03 -0500

    added_binance_nodejs_project
    also restructured directory by adding a folder for each project

---
+++ scalpbot-node-binance/node-binance-api-master/examples/example.js
@@ -0,0 +1,143 @@
+var action = 6;
+
+const binance = require('../node-binance-api.js');
+binance.options({
+  'APIKEY':'YYrAdvp6cfJPAIJg8kTgT0Qd7HxVgrEQm4vJ9lU1HKZqEONt61ZDAVA1s7mnnSjD',
+  'APISECRET':'AwGen4zXvDa0P2Q1ernpawQ6LlOeUY3ITcNZLG6FPhOzH1sM0u1UMiBTXacZaZLW'
+});
+
+console.log("\r\nStarting: ScalpBot for Binance... \r\n");
+
+(function() {
+
+	// Maintain Market Depth Cache Locally via WebSocket
+	binance.websockets.depthCache(["ICXETH"], function(symbol, depth) {
+		let max = 1; // Show 10 closest orders only
+		let bids = binance.sortBids(depth.bids, max);
+		let asks = binance.sortAsks(depth.asks, max);
+		console.log("ICXETH"+" depth cache update");
+		console.log("asks", asks);
+		console.log("bids", bids);
+		console.log("ask: "+binance.first(asks));
+		console.log("bid: "+binance.first(bids));	
+		console.log("\r\n\n");			
+	});
+
+	switch(action)
+	{
+		case 1:
+			// Getting latest price of ICX/ETH
+			binance.prices(function(ticker) {
+				//console.log("prices()", ticker);
+				console.log("Price of ICX/ETH: ", ticker.ICXETH);
+			});		
+			break;
+		case 2:
+			// Getting bid/ask prices for ICX/ETH
+			binance.bookTickers(function(ticker) {
+				console.log("bookTickers()", ticker.ICXETH);
+				//console.log("Price of ICX/ETH: \r\n", ticker.ICXETH);
+			});
+			break;
+		case 3:
+			// test connection
+			binance.serverTime(function(response) {
+				console.log(response);
+			});
+			break;
+		case 4:
+			binance.pingServer(function(response) {
+				console.log(response);
+			});
+			break;
+		case 5:
+			binance.depthv2("ICXETH", function(data) {
+				console.log("market depth", data);
+			}, 20);
+			break;
+		case 6:
+			console.log("heartbeat.. \r\n");
+	}
+	setTimeout(arguments.callee, 2000);
+})();
+
+// Get bid/ask prices
+//binance.allBookTickers(function(json) {
+//  console.log("allBookTickers",json);
+//});
+
+// Getting list of current balances
+//binance.balance(function(balances) {
+	//console.log("balances()", balances);
+	//if ( typeof balances.ETH !== "undefined" ) {
+		//console.log("ETH balance: ", balances.ETH.available);
+	//}
+//});
+
+// Getting bid/ask prices for a symbol
+//binance.bookTickers(function(ticker) {
+//	console.log("bookTickers()", ticker);
+//	console.log("Price of BNB: ", ticker.BNBBTC);
+//});
+
+// Get market depth for a symbol
+//binance.depth("SNMBTC", function(json) {
+//	console.log("market depth",json);
+//});
+
+// Getting list of open orders
+//binance.openOrders("ETHBTC", function(json) {
+//	console.log("openOrders()",json);
+//});
+
+// Check an order's status
+//let orderid = "7610385";
+//binance.orderStatus("ETHBTC", orderid, function(json) {
+//	console.log("orderStatus()",json);
+//});
+
+// Cancel an order
+//binance.cancel("ETHBTC", orderid, function(response) {
+//	console.log("cancel()",response);
+//});
+
+// Trade history
+//binance.trades("SNMBTC", function(json) {
+//  console.log("trade history",json);
+//});
+
+// Get all account orders; active, canceled, or filled.
+//binance.allOrders("ETHBTC", function(json) {
+//	console.log(json);
+//});
+
+//Placing a LIMIT order
+//binance.buy(symbol, quantity, price);
+//binance.buy("ETHBTC", 1, 0.0679);
+//binance.sell("ETHBTC", 1, 0.069);
+
+//Placing a MARKET order
+//binance.buy(symbol, quantity, price, type);
+//binance.buy("ETHBTC", 1, 0, "MARKET")
+//binance.sell(symbol, quantity, 0, "MARKET");
+
+// Periods: 1m,3m,5m,15m,30m,1h,2h,4h,6h,8h,12h,1d,3d,1w,1M
+//binance.candlesticks("BNBBTC", "5m", function(ticks) {
+	//console.log("candlesticks()", ticks);
+	//let last_tick = ticks[ticks.length - 1];
+	//let [time, open, high, low, close, volume, closeTime, assetVolume, trades, buyBaseVolume, buyAssetVolume, ignored] = last_tick;
+	//console.log("BNBBTC last close: "+close);
+//});
+
+
+// Maintain Market Depth Cache Locally via WebSocket
+//binance.websockets.depthCache(["BNBBTC"], function(symbol, depth) {
+	//let max = 10; // Show 10 closest orders only
+	//let bids = binance.sortBids(depth.bids, max);
+	//let asks = binance.sortAsks(depth.asks, max);
+	//console.log(symbol+" depth cache update");
+	//console.log("asks", asks);
+	//console.log("bids", bids);
+	//console.log("ask: "+binance.first(asks));
+	//console.log("bid: "+binance.first(bids));
+//});
